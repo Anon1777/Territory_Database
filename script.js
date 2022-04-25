0c4d7d30340b076426bd90baf119fcf939dec259
Anon1777 <joetenace100@gmail.com>
2022-04-25 19:12:41 -0400

    second commit

--- script.js
+++ script.js
@@ -453,3 +453,30 @@ var array = [
     "",
     ""
 ]
+
+function autocomplete(inp, arr){
+    var currentFocus;
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        closeAllLists();
+        if (!val) {
+            return false;
+        }
+        currentFocus = -1;
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        this.parentNode.appendChild(a);
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                b = document.createElement("DIV");
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                b.addEventListener("click", function(e){
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                })
+            }
+        }
+    })
+}
