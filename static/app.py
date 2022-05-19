90b03bece751baa21ad8f1b9e3ad4087a722b10c
jospehg farrell <224jfarrell@frhsd.com>
2022-05-19 14:25:30 -0400

    twenty-second commit

--- static/app.py
+++ static/app.py
@@ -3,8 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("Page/index.html")
+    return render_template("../templates/index.html")
 
 @app.route("/austria")
 def austria():
-    return render_template("Page/austria.html")
+    return render_template("../templates/austria.html")
+
+@app.route("/afghanistan")
+def afghanistan():
+    return render_template("../templates/afghanistan.html")
