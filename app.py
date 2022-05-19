c51cf73f5a1f55231b7f7eccd8d392d892b2de81
jospehg farrell <224jfarrell@frhsd.com>
2022-05-19 14:01:39 -0400

    twenty-first commit redo PLEASE DONT BREAK

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("Page/index.html")
+
+#@app.route("/austria")
+#def austria():
+#    return render_template("Page/austria.html")
